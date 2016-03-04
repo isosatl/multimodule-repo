2cac4e5cc8a770973ef0d51d270e33909b369049
Isaias Sosa <isosa@isosa-net.isosa>
2016-03-04 17:00:36 -0600

    Commiting all the structure created through the archetype

---
+++ bundle/src/main/java/multimodule-pack/package-info.java
@@ -0,0 +1,4 @@
+@Version("1.0.0")
+package multimodule-pack;
+
+import aQute.bnd.annotation.Version;
---
+++ bundle/src/test/java/multimodule-pack/SimpleUnitTest.java
@@ -0,0 +1,14 @@
+package multimodule-pack;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleUnitTest {
+
+    @Test
+    public void someTest() {
+        assertTrue(true);
+    }
+
+}
