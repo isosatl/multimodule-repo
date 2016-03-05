d18a16ca2f963a6dae897ec394d44af2b28dcc42
Isaias Sosa <isosa@isosa-net.isosa>
2016-03-05 05:16:06 -0600

    Activator.java

---
+++ bundle/src/main/java/multimodule/com/Activator.java
@@ -0,0 +1,23 @@
+package multimodule.com;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: isosa
+ * Date: 3/5/16
+ * Time: 5:13 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Activator implements BundleActivator{
+    @Override
+    public void start(BundleContext bundleContext) throws Exception {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void stop(BundleContext bundleContext) throws Exception {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
